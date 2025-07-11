f3fef0c890aaf50f52295404d18889edc5736e6f
Masahiko Sato <masahills@gmail.com>
2025-07-12 00:46:34 +0900

    Refactor MapWideSymbol class to improve readability by renaming constant and updating array values

--- src/main/java/com/iciao/kanada/maps/MapWideSymbol.java
+++ src/main/java/com/iciao/kanada/maps/MapWideSymbol.java
@@ -8,25 +8,25 @@ import com.iciao.kanada.JMapper;
  * @author Masahiko Sato
  */
 /*
- 	    0	1	2	3	4	5	6	7	8	9	A	B	C	D	E	F
 U+300x	　	、	。	〃	〄	々	〆	〇	〈	〉	《	》	「	」	『	』
 U+301x	【	】	〒	〓	〔	〕	〖	〗	〘	〙	〚	〛	〜	〝	〞	〟
 U+302x	〠	〡	〢	〣	〤	〥	〦	〧	〨	〩	〪	〫	〬	〭	〮	〯
 U+303x	〰	〱	〲	〳	〴	〵	〶	〷	〸	〹	〺	〻	〼	〽	 〾 	〿
  */
 public class MapWideSymbol extends JMapper {
-    private static final String[] cjkSymbolsAndPunctuationToAscii = {
-            " ", ",", ".", "(repeat)", "(jis)", "(repeat)", "shime", "(circle)",
-            "<", ">", "<<", ">>", "\"", "\"", "\"", "\"",
+    private static final String[] CJK_SYMBOLS_AND_PUNCTUATION_TO_ASCII = {
+            " ", ",", ".", "(repeat)", "(jis)", "(repeat)", "(closing mark)", "(circle)",
+            "<", ">", "<<", ">>", "'", "'", "\"", "\"",
 
-            "[", "]", "(postal mark)", "=", "[", "]", "[", "]",
-            "[", "]", "[", "]", "~", "\"", "\"", "\"",
+            "[", "]", "(postal mark)", "=", "[", "]", "{", "}",
+            "[", "]", "[[", "]]", "~", "\"", "\"", "\"",
 
             "(postal mark face)", "1", "2", "3", "4", "5", "6", "7",
             "8", "9", "", "", "", "", "", "",
 
             "~", "(repeat)", "(repeat)", "(repeat)", "(repeat)", "(repeat)", "(postal mark circle)", "XX",
-            "10", "11", "12", "(repeat)", "(square)", "^", " "};
+            "10", "20", "30", "(repeat)", "(masu mark)", "♪", " ", " "
+    };
 
     @Override
     protected void process(String str, int param) {
@@ -34,7 +34,7 @@ public class MapWideSymbol extends JMapper {
         int thisChar = str.codePointAt(0);
         Character.UnicodeBlock block = Character.UnicodeBlock.of(thisChar);
         if (block == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION) {
-            out.append(cjkSymbolsAndPunctuationToAscii[thisChar - 0x3000]);
+            out.append(CJK_SYMBOLS_AND_PUNCTUATION_TO_ASCII[thisChar - 0x3000]);
         } else {
             out.appendCodePoint(thisChar);
         }
