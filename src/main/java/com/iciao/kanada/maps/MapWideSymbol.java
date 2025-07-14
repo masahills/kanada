945f18745363c635d3dc2065907c77b1eb271916
Masahiko Sato <masahills@gmail.com>
2025-07-14 14:10:04 +0900

    Refactor MapWideSymbol class to streamline CJK symbol processing and enhance half symbol conversion

--- src/main/java/com/iciao/kanada/maps/MapWideSymbol.java
+++ src/main/java/com/iciao/kanada/maps/MapWideSymbol.java
@@ -29,13 +29,6 @@ public class MapWideSymbol extends JMapper {
             "10", "20", "30", "(repeat)", "(masu mark)", "♪", " ", " "
     };
 
-    private static final String[] CJK_SYMBOLS_AND_PUNCTUATION_TO_HALF_SYMBOL = {
-            " ", "､", "｡", "〃", "〄", "々", "〆", "〇", "〈", "〉", "《", "》", "｢", "｣", "『", "』",
-            "【", "】", "〒", "〓", "〔", "〕", "〖", "〗", "〘", "〙", "〚", "〛", "〜", "〝", "〞", "〟",
-            "〠", "〡", "〢", "〣", "〤", "〥", "〦", "〧", "〨", "〩", "〪", "〫", "〬", "〭", "〮", "〯",
-            "〰", "〱", "〲", "〳", "〴", "〵", "〶", "〷", "〸", "〹", "〺", "〻", "〼", "〽", "〾", "〿"
-    };
-
     public MapWideSymbol(Kanada kanada) {
         super(kanada);
     }
@@ -43,14 +36,14 @@ public class MapWideSymbol extends JMapper {
     @Override
     protected void process(String str, int param) {
         StringBuilder out = new StringBuilder();
-        int thisChar = str.codePointAt(0);
+        char thisChar = str.charAt(0);
         Character.UnicodeBlock block = Character.UnicodeBlock.of(thisChar);
         if (block == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION) {
             int index = thisChar - 0x3000;
             if (param == JMapper.TO_ASCII) {
                 out.append(CJK_SYMBOLS_AND_PUNCTUATION_TO_ASCII[index]);
             } else if (param == JMapper.TO_HALF_SYMBOL) {
-                out.append(CJK_SYMBOLS_AND_PUNCTUATION_TO_HALF_SYMBOL[index]);
+                out.append(cjkSymbolsAndPunctuationToHalfSymbol(thisChar));
             } else {
                 out.appendCodePoint(thisChar);
             }
@@ -58,5 +51,25 @@ public class MapWideSymbol extends JMapper {
             out.appendCodePoint(thisChar);
         }
         setString(out.toString());
+    }
+
+    private String cjkSymbolsAndPunctuationToHalfSymbol(char ch) {
+        switch (ch) {
+            case '　':
+                return " "; // space
+            case '、':
+                return "､"; // comma
+            case '。':
+                return "｡"; // period
+            case '「':
+                return "｢"; // left bracket
+            case '」':
+                return "｣"; // right bracket
+            case '〜':
+            case '〰':
+                return "ｰ"; // long vowel mark
+            default:
+                return String.valueOf(ch);
+        }
     }
 }
