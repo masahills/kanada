a85abee625b6a5f56f0abee9338f8fee241bde1f
Masahiko Sato <masahills@gmail.com>
2025-09-22 11:29:31 +0900

    Refactor KanadaTest.java: add tests for uppercasing and capitalizing romaji-only text processing

--- src/test/java/com/iciao/kanada/KanadaTest.java
+++ src/test/java/com/iciao/kanada/KanadaTest.java
@@ -56,6 +56,34 @@ public class KanadaTest {
     }
 
     @Test
+    public void testUppercasedAlphabets() throws Exception {
+        String result = romaji.upperCaseAll().process("漢字 kanji@example.com");
+        assertNotNull(result);
+        assertEquals("KANJI KANJI@EXAMPLE.COM", result);
+    }
+
+    @Test
+    public void testCapitalizeAlphabets() throws Exception {
+        String result = romaji.upperCaseFirst().process("漢字 kanji@example.com");
+        assertNotNull(result);
+        assertEquals("Kanji Kanji@example.com", result);
+    }
+
+    @Test
+    public void testUppercasedRomajiOnly() throws Exception {
+        String result = romaji.upperCaseAll().upperCaseRomajiOnly().process("漢字 kanji@example.com");
+        assertNotNull(result);
+        assertEquals("KANJI kanji@example.com", result);
+    }
+
+    @Test
+    public void testCapitalizeRomajiOnly() throws Exception {
+        String result = romaji.upperCaseFirst().upperCaseRomajiOnly().process("漢字 kanji@example.com");
+        assertNotNull(result);
+        assertEquals("Kanji kanji@example.com", result);
+    }
+
+    @Test
     public void testBasicRomajiConversion() throws Exception {
         String result = romaji.process("漢字");
         assertNotNull(result);
@@ -133,6 +161,19 @@ public class KanadaTest {
         String tenji = """
                 ⠀⠀⠤⠰⠠⠝⠤⠄⠀⠤⠦⠠⠝⠊⠓⠕⠝⠛⠕⠴⠀⠶⠇⠮⠴⠐⠪⠶⠤⠰⠀
                 ⠤⠦⠠⠝⠑⠺⠴⠀⠶⠁⠕⠑⠳⠃⠶⠤⠔⠀⠁⠑⠄⠳⠵⠹⠲
+                """;
+        assertEquals(text, hiragana.process(tenji));
+    }
+
+    @Test
+    public void testBrailleInput4() throws Exception {
+        String text = """
+                  こんかい さいよー された しんごの うち\s
+                かたかなごが じつに 4わり ちかくを しめた。\s
+                """;
+        String tenji = """
+                ⠀⠀⠪⠴⠡⠃⠀⠱⠃⠜⠒⠀⠱⠛⠕⠀⠳⠴⠐⠪⠎⠀⠉⠗⠀
+                ⠡⠕⠡⠅⠐⠪⠐⠡⠀⠐⠳⠝⠇⠀⠼⠙⠄⠓⠀⠗⠡⠩⠔⠀⠳⠿⠕⠲⠀⠀
                 """;
         assertEquals(text, hiragana.process(tenji));
     }
